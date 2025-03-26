9965f55d2a40d996bc56caab15b478f4d17996ab
yannainglin <github.yannainglin@gmail.com>
2025-03-26 17:17:51 +0800

    add https infront

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const app = new Hono();
 const allowedOrigins = new Set([
   "http://localhost:5173",
   "https://api.rapid-achiever.sxplab.com",
-  "blazenote-api.blazehack-yannaing-lin.workers.dev"
+  "https://blazenote-api.blazehack-yannaing-lin.workers.dev"
 
 ]);
 
