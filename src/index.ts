aa3442bb5c45c64227efa6c151b92aa2d4c40dd6
yannainglin <github.yannainglin@gmail.com>
2025-03-26 17:16:14 +0800

    add fqdns

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,8 @@ const app = new Hono();
 const allowedOrigins = new Set([
   "http://localhost:5173",
   "https://api.rapid-achiever.sxplab.com",
-  "https://rapid-achiever.sxplab.com'"
+  "blazenote-api.blazehack-yannaing-lin.workers.dev"
+
 ]);
 
 // https://hono.dev/docs/middleware/builtin/cors
